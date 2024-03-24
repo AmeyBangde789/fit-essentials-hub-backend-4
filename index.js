efb4c24efe59bab78392a3c6ce7e25c9c5543f26
AmeyBangde <ameybangde69@gmail.com>
2024-03-24 07:26:01 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port=process.env.PORT
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:4200',
+    origin:'https://mean-stack-project-frontend.onrender.com',
     credentials: true
 }))
 app.use("/api/route", routeRoute);
